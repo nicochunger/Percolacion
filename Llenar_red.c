fd3629199fbf2e6984b62051014a97993458ff5c
Nicolas Unger <nicounger94@gmail.com>
2017-04-03 17:48:27 -0300

    Declaracion de las funciones

---
+++ Llenar_red.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+/* Declaracion de Funciones*/
+
+void llenar(int *red, int m, int n, float proba);
+void imprimir(int *red, int m, int n);
+
+/* Definicion de Funciones */
+
+void llenar(int *red, int m, int n, float proba){
+
+}
+
+void imprimir(int *red, int m, int n){
+
+}
+
+}
+
+
+
+
